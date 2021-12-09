cc4d5d5da29472eb27c13d084853751e3f6e1f4b
Joy Edem <koks.edem@gmail.com>
2021-12-08 22:28:36 -0500

    created post request to add book to list based on parameters: `book_title` and `book_author`

--- books-api/books.py
+++ books-api/books.py
@@ -22,3 +22,14 @@ async def get_all_books(skip_book: Optional[str] = None):
 @app.get("/{book_name}")
 async def get_book(book_name: str):
   return BOOKS[book_name]
+
+@app.post("/")
+async def create_book(book_title, book_author):
+  current_book_id = 0 
+  if len(BOOKS) > 0:
+    for book in BOOKS:
+      x = int(book.split("_")[-1])
+      if x > current_book_id:
+        current_book_id = x
+  BOOKS[f'book_{current_book_id + 1}'] = {'title': book_title, 'author': book_author}
+  return BOOKS[f'book_{current_book_id + 1}']  
